978dce0500203e4521e8ec25618c56c3e9fb9012
3str-team <104576480+3str-team@users.noreply.github.com>
2022-07-10 09:10:19 +0300

    Footer ~ complete

--- js/contacts.js
+++ js/contacts.js
@@ -1,6 +1,6 @@
 const email = document.querySelector("header .info .contacts .links .email");
 
-let copyEmail = () => {
+const copyEmail = () => {
   navigator.clipboard.writeText("Mirza.Bichurin@novsu.ru");
   new Log("E-mail скопирован в буфер обмена");
 };
