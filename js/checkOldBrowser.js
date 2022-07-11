c36cf6de6800aea0aad9bac0cdf002c8c3009e75
3str-team <104576480+3str-team@users.noreply.github.com>
2022-07-11 19:02:02 +0300

    Исправлена ошибка в тексте

--- js/checkOldBrowser.js
+++ js/checkOldBrowser.js
@@ -10,7 +10,7 @@ for (let i = 0; i < arrayOfProps.length; ++i) {
   let prop = arrayOfProps[i];
   if (Modernizr[prop] == undefined || Modernizr[prop] == false) {
     new Log({
-      text: "Ваш браузер устарел. Это може привести к некорректному отображению сайта. Пожалуйста, обновитесь",
+      text: "Ваш браузер устарел. Это может привести к некорректному отображению сайта. Пожалуйста, обновитесь",
       duration: 3000,
       isErr: true,
       infinity: true
